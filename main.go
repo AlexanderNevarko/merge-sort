e9c175a650fd946ef1d330a694a410054a56a978
Alexander Nevarko <nevarko.ad@phystech.edu>
2019-12-23 00:22:22 +0300

    Add correctness check

--- main.go
+++ main.go
@@ -18,9 +18,18 @@ func main() {
 	arrSize := *arrSizePtr
 
 	arr := rand.Perm(arrSize)
+
 	start := time.Now()
 	array := mergesort.Mergesort(arr, numProc)
 	duration := time.Since(start)
 
 	fmt.Printf("time spent for parallel mergesort: %s\n", duration)
+	for i, value := range array {
+		if i == len(array)-1 {
+			break
+		}
+		if value > array[i+1] {
+			fmt.Printf("Error in mergesort occured!\n")
+		}
+	}
 }
